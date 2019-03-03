b53b2f9bcb6a92f3a562602a842aff6d3117ebf0
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-03-03 19:29:00 +0900

    implement Room model

---
+++ app/models/room.rb
@@ -0,0 +1,33 @@
+class Room < ApplicationRecord
+  # association
+
+  # validation
+  VALID_CODE_REGEX = /\A[\w-]+\z/ # a-z A-Z 0-9 _ -
+  validates :code,
+    presence: true,
+    uniqueness: true,
+    length: { in: 1..255 },
+    format: { with: VALID_CODE_REGEX}
+  validates :name,
+    length: { maximum: 255 }
+
+  # クラスメソッド
+
+  # a-zA-Z0-9_-から成る8文字の文字列を返す
+  def self.random_code
+    SecureRandom.urlsafe_base64(nil, false)[0..7]
+  end
+
+
+  # インスタンスメソッド
+
+  # codeのみについて有効かどうかをbooleanで返す
+  def has_valid_code?
+    self.errors.clear
+    self.valid?
+    result = self.errors.messages[:code].count.zero?
+    self.errors.clear
+    return result
+  end
+
+end
